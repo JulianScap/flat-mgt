eb27910ad19a0008492957a90594041f3db3d071
Julian Adler <julian.a@passport.com>
2018-04-04 12:38:24 +1200

    data init script

--- FlatManagement.Database/OtherScripts/InitData.sql
+++ FlatManagement.Database/OtherScripts/InitData.sql
@@ -1,1 +1,10 @@
-﻿
+﻿INSERT INTO ref.PeriodType (PeriodTypeId, [Name]) VALUES (1, 'Daily');
+INSERT INTO ref.PeriodType (PeriodTypeId, [Name]) VALUES (2, 'Weekly');
+INSERT INTO ref.PeriodType (PeriodTypeId, [Name]) VALUES (3, 'Fortnightly');
+INSERT INTO ref.PeriodType (PeriodTypeId, [Name]) VALUES (4, 'Monthly');
+INSERT INTO ref.PeriodType (PeriodTypeId, [Name]) VALUES (5, 'Quarterly');
+INSERT INTO ref.PeriodType (PeriodTypeId, [Name]) VALUES (6, 'Biannually');
+INSERT INTO ref.PeriodType (PeriodTypeId, [Name]) VALUES (7, 'Annually');
+INSERT INTO ref.PeriodType (PeriodTypeId, [Name]) VALUES (8, 'Always');
+INSERT INTO ref.PeriodType (PeriodTypeId, [Name]) VALUES (9, 'Rarely');
+INSERT INTO ref.PeriodType (PeriodTypeId, [Name]) VALUES (10, 'Seasonally');
