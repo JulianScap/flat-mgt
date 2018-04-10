002cee1b1ef01be1190ca69a214273a38c135880
Julian Adler <julian.a@passport.com>
2018-04-11 11:30:25 +1200

    init angular project

---
+++ FlatManagement.Web/Controllers/HomeControler.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlatManagement.Web.Controllers
+{
+	public class HomeControler : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Error()
+		{
+			ViewData["RequestId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+			return View();
+		}
+	}
+}
