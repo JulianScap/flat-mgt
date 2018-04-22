0b92b927f1462f9743c30653a265b2ad97be3233
Julian Adler <julian.a@passport.com>
2018-04-22 12:27:59 +1200

    blocked read / write operations on period type table

--- FlatManagement.Dal/Impl/PeriodTypeDataAccess.cs
+++ FlatManagement.Dal/Impl/PeriodTypeDataAccess.cs
@@ -1,4 +1,6 @@
-﻿using FlatManagement.Dal.Interface;
+﻿using FlatManagement.Common.Dto;
+using FlatManagement.Common.Exceptions;
+using FlatManagement.Dal.Interface;
 using FlatManagement.Dal.Tools;
 using FlatManagement.Dto.Entities;
 using Microsoft.Extensions.Configuration;
@@ -10,6 +12,21 @@ namespace FlatManagement.Dal.Impl
 		public PeriodTypeDataAccess(IConfiguration configuration) : base(configuration)
 		{
 
+		}
+
+		public override void Delete(IDto item)
+		{
+			throw new DisabledOperationException();
+		}
+
+		public override void Insert(PeriodType item)
+		{
+			throw new DisabledOperationException();
+		}
+
+		public override void Update(PeriodType item)
+		{
+			throw new DisabledOperationException();
 		}
 	}
 }
