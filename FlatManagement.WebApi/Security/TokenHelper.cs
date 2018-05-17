5c75bc3dd6aa85e0f09fc867516a0b228872a9cd
Julian Adler <julian.a@passport.com>
2018-05-17 22:46:48 +1200

    the token generation now uses configuration

--- FlatManagement.WebApi/Security/TokenHelper.cs
+++ FlatManagement.WebApi/Security/TokenHelper.cs
@@ -1,33 +1,43 @@
 ﻿using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
 namespace FlatManagement.WebApi.Security
 {
 	public class TokenHelper
 	{
-		private const string key = "mLBMceMJwD7uzHKXzk2vA5qmqlx9ms2sTjsuSr6tGCXvefjoZlh0dmKPkCj7U0lXU8sMA23yC7iAJxki0DB0Xk+1lrt5v3+zLZGeY3ZYu7bZ2HWsEBX/SOBAPZGpWwHeGe4MFsdk5tUo2B+0v2cAHbgM/7s0Jw0K9s4iGo1201U=";
+		private static string symmetricSecurityKey;
+		private static string audience;
+		private static string issuer;
+
+		public static void Configure(IConfiguration configuration)
+		{
+			symmetricSecurityKey = configuration["Security:Jwt:SymmetricSecurityKey"];
+			audience = configuration["Security:Jwt:Audience"];
+			issuer = configuration["Security:Jwt:Issuer"];
+		}
 
 		public static string GetNewToken(string login, string profile)
 		{
 			JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
 
-			SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Convert.FromBase64String(key));
+			SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Convert.FromBase64String(symmetricSecurityKey));
 			DateTime utcNow = DateTime.UtcNow;
 			DateTime expires = utcNow.AddMinutes(60);
 			SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
 			{
 				Subject = new ClaimsIdentity(new Claim[]
-					{
-							new Claim(ClaimTypes.Name, login),
-							new Claim(ClaimTypes.Role, profile)
-					}),
+				{
+						new Claim(ClaimTypes.Name, login),
+						new Claim(ClaimTypes.Role, profile)
+				}),
 				Expires = expires,
-				Audience = "http://fm.web.local:4200/",
+				Audience = audience,
 				IssuedAt = utcNow,
 				NotBefore = utcNow,
-				Issuer = "http://fm.api.local/",
+				Issuer = issuer,
 				SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature),
 			};
 
