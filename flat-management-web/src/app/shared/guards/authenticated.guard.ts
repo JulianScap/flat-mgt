364b0a70f406dcb2dc958b8a08e2d2bf2a8bbfb3
Julian Adler <julian.a@passport.com>
2018-05-18 17:01:09 +1200

    added logs and redirection if the user is not logged in

--- flat-management-web/src/app/shared/guards/authenticated.guard.ts
+++ flat-management-web/src/app/shared/guards/authenticated.guard.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { SessionManager } from '../services/session-manager.service';
 
 @Injectable()
 export class AuthenticatedGuard implements CanActivate {
-  constructor(private sessionManager: SessionManager) { }
+  constructor(private sessionManager: SessionManager,
+    private router: Router) { }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return this.sessionManager.isAuthenticated();
+    if (this.sessionManager.isAuthenticated()) {
+      return true;
+    }
+    else {
+      console.log('logged out, exiting');
+      this.router.navigate(['/authenticate']);
+      return false;
+    }
   }
 }
